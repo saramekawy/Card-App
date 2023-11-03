2d22858cb0380c47aa704f3b0d3fe4aeef528c8c
Sara Mekawy <as1113090@gmail.com>
2023-11-03 10:45:46 +0200

    image background

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/birthday_card.png"),
+                fit: BoxFit.cover)),
+      ),
+    );
+  }
+}
